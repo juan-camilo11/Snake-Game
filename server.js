bfff1858f7162a36a1e4aba13f59def394dda5bd
Juan Varon <juancamilo_11@utexas.edu>
2022-01-04 23:18:26 -0600

    had forgotten one thing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/snake-game'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/snake-game/index.html'));});
